88c828b766c2780a0ca0cc393ee543f574ed7834
Osman Gormus <osman@gorm.us>
2026-04-11 23:25:35 +0200

    chore: update theme skeleton and dependencies

--- spec/system/core_features_spec.rb
+++ spec/system/core_features_spec.rb
@@ -8,20 +8,8 @@
 # search, search:quick_search, search:full_page
 #
 # For more details, see https://meta.discourse.org/t/-/361381
-RSpec.describe "Core features", type: :system do
+RSpec.describe "Core features" do
   before { upload_theme_or_component }
 
-  it_behaves_like "having working core features",
-                  skip_examples: %i[
-                    login
-                    likes
-                    profile
-                    topics
-                    topics:read
-                    topics:reply
-                    topics:create
-                    search
-                    search:quick_search
-                    search:full_page
-                  ]
+  it_behaves_like "having working core features"
 end
